1facd220bef21f2d1523b150b201c3d1098a411c
Nikodem <nikodem.lorenz@gmail.com>
2019-08-01 13:31:30 +0200

    Updates

--- sw.js
+++ sw.js
@@ -2,10 +2,6 @@ importScripts('/portfolio/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/portfolio/_nuxt/0b38330dc83cf7e5ae02.js",
-    "revision": "ce7341000dbfe4a7db6766cda7bfa1ad"
-  },
-  {
     "url": "/portfolio/_nuxt/0ceb99c8aba00d16b054.js",
     "revision": "93d3557fc2ee62623e09ad98c3f9a8bd"
   },
@@ -14,8 +10,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "e0d28da3dbea581406727e7d8acc29be"
   },
   {
-    "url": "/portfolio/_nuxt/5f6b8fc7541ff16f5ca9.js",
-    "revision": "c28756cc100ff59a2c53c58f32fefee7"
+    "url": "/portfolio/_nuxt/3f7c8cb3dab332f1e2c6.js",
+    "revision": "333cbd9726a4009d81378915e373a401"
+  },
+  {
+    "url": "/portfolio/_nuxt/72d09f9fe0fad315db6d.js",
+    "revision": "db8e00ca8139c799f00e7b0fe87f0d67"
   },
   {
     "url": "/portfolio/_nuxt/ae47e0cb0a3ece82e61b.js",
