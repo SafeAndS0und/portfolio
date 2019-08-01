2ff12ed17a6f2064d9b32871b605434e038487c3
Nikodem <nikodem.lorenz@gmail.com>
2019-08-01 13:00:23 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,35 @@
+importScripts('/portfolio/_nuxt/workbox.4c4f5ca6.js')
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "/portfolio/_nuxt/0ceb99c8aba00d16b054.js",
+    "revision": "93d3557fc2ee62623e09ad98c3f9a8bd"
+  },
+  {
+    "url": "/portfolio/_nuxt/0dfaf18f5870c08b5135.js",
+    "revision": "e0d28da3dbea581406727e7d8acc29be"
+  },
+  {
+    "url": "/portfolio/_nuxt/ae47e0cb0a3ece82e61b.js",
+    "revision": "61d076d610435fdf9c70279d7ceef73c"
+  },
+  {
+    "url": "/portfolio/_nuxt/d39bfe6311f3f38c0287.js",
+    "revision": "620341409a18e6a2da6adb6fe13fa1b1"
+  },
+  {
+    "url": "/portfolio/_nuxt/facbbac99a06952e639f.js",
+    "revision": "364749f74b196279cf3c02ecf0652929"
+  }
+], {
+  "cacheId": "portfolio",
+  "directoryIndex": "/",
+  "cleanUrls": false
+})
+
+workbox.clientsClaim()
+workbox.skipWaiting()
+
+workbox.routing.registerRoute(new RegExp('/portfolio/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+
+workbox.routing.registerRoute(new RegExp('/portfolio/.*'), workbox.strategies.networkFirst({}), 'GET')
