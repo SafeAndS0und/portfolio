ab379bed1e97e64dc52185c64df0e72ad9c1bdae
Nikodem <nikodem.lorenz@gmail.com>
2019-08-08 14:41:55 +0200

    Updates

--- sw.js
+++ sw.js
@@ -2,24 +2,28 @@ importScripts('/portfolio/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/portfolio/_nuxt/0ceb99c8aba00d16b054.js",
-    "revision": "93d3557fc2ee62623e09ad98c3f9a8bd"
+    "url": "/portfolio/_nuxt/3a661e99c36b091b03c7.js",
+    "revision": "d8a68345c866ca824746822c8dc05852"
   },
   {
-    "url": "/portfolio/_nuxt/0dfaf18f5870c08b5135.js",
-    "revision": "e0d28da3dbea581406727e7d8acc29be"
+    "url": "/portfolio/_nuxt/41a281e250edd4194302.js",
+    "revision": "c0a225b9d91cc18d9fefd4fced87b4cc"
   },
   {
-    "url": "/portfolio/_nuxt/9d44d686321d9e963784.js",
-    "revision": "9396190dda51741aec6303d3557bd0d2"
+    "url": "/portfolio/_nuxt/4b332a2050ba96a34c1c.js",
+    "revision": "823eb1ed80285b5e1668d8dfea0425ba"
+  },
+  {
+    "url": "/portfolio/_nuxt/8d713e5fec370546a069.js",
+    "revision": "7c0ba53e500f7ee370ec9c40f3678da6"
   },
   {
     "url": "/portfolio/_nuxt/ae47e0cb0a3ece82e61b.js",
     "revision": "61d076d610435fdf9c70279d7ceef73c"
   },
   {
-    "url": "/portfolio/_nuxt/fd7c55544852cbcfad0f.js",
-    "revision": "b90c8ac007985908a8fefd3ba46d6a38"
+    "url": "/portfolio/_nuxt/f0266ed2a0fba660f3e4.js",
+    "revision": "0941013b045e318410e81b42676e5ccb"
   }
 ], {
   "cacheId": "portfolio",
